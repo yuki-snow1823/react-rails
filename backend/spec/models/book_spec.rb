464f27e155cda1e1bb8ffc98f5e36306cfa5a7ec
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-12-25 10:33:34 +0900

    rspec setting

--- backend/spec/models/book_spec.rb
+++ backend/spec/models/book_spec.rb
@@ -1,5 +1,12 @@
+require "rails_helper"
 require 'spec_helper'
 
 describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '1文字のタイトルは保存されない' do
+    book = Book.new(title: 'h')
+    expect(book).to be_invalid
+
+    book.title = 'hoge'
+    expect(book).to be_valid
+  end
 end
