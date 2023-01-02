39a7ac0f6c5283cd7f6830186d02591d91c6ef42
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2023-01-02 11:37:35 +0900

    graphqlでデータ取得できるまで

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,5 +23,9 @@ module Sample
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Rails7だと/graphiqlにアクセスするとエラーが出るため
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
